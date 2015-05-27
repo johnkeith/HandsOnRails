10d7eeb1718a2baf3bbf8496fbdd6064cedef435
John Keith <johnkeith@outlook.com>
2015-05-26 20:38:46 -0400

    added discussion posts actions and index view behind authentication wall

--- app/controllers/discussion_posts_controller.rb
+++ app/controllers/discussion_posts_controller.rb
@@ -1,2 +1,26 @@
 class DiscussionPostsController < ApplicationController
+	before_action :authenticate_user!
+
+	def index
+	end
+
+	def show
+	end
+
+	def create
+	end
+
+	def new
+	end
+
+	def edit
+		# GET 
+	end
+
+	def destroy
+	end
+
+	def update
+		# POST
+	end
 end
